aa8fd4225c5c704895f2dbb02e5313d2abb1e918
johnson <johnsonle2929@gmail.com>
2022-05-25 14:20:54 -0700

    adding loading state

--- src/App.js
+++ src/App.js
@@ -6,43 +6,24 @@ import './App.css';
 function App() {
 
   const [movies, setMovies] = useState([])
-  // const fetchFilms = () => {
-  //   const endpoint = `https://swapi.dev/api/films/`;
-  //   fetch(endpoint)
-  //     .then((response) => {
-  //       return response.json()
-  //     })
-  //     .then((data) => {
-  //       // You can transform your data from the API to match your props 
-  //       const transformData = data.results.map(movieData => {
-  //         return {
-  //           id: movieData.episode_id,
-  //           title: movieData.title,
-  //           releaseDate: movieData.release_date,
-  //           openingText: movieData.opening_crawl
-  //         }
-  //       }) 
-  //       setMovies(transformData)
-  //     })
-  // };
-
+  const [isLoading, setIsLoading] = useState(false)
   // async await way of fetching data 
   const fetchFilms = async () => {
+    setIsLoading(true)
     const endpoint = `https://swapi.dev/api/films/`;
     const data = await (await fetch(endpoint)).json()
-
    
-        // You can transform your data from the API to match your props 
-        const transformData = data.results.map(movieData => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            releaseDate: movieData.release_date,
-            openingText: movieData.opening_crawl
-          }
-        }) 
-        setMovies(transformData)
-      
+    // You can transform your data from the API to match your props 
+    const transformData = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        releaseDate: movieData.release_date,
+        openingText: movieData.opening_crawl
+      }
+    }) 
+    setMovies(transformData)
+    setIsLoading(false)
   };
 
   return (
@@ -51,7 +32,12 @@ function App() {
         <button onClick={fetchFilms}>Fetch Movies</button>
       </section>
       <section>
+        {isLoading && 
+        <p>Loading .... </p>
+        }        
+        {!isLoading &&
         <MoviesList movies={movies} />
+        }
       </section>
     </React.Fragment>
   );
